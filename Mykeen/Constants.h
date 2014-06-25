615bbd29840627ecb325305a0650941b377b595d
adsurbum <adsurbum@gmail.com>
2014-06-25 13:49:06 +0300

    admob playing

--- Mykeen/Constants.h
+++ Mykeen/Constants.h
@@ -8,5 +8,7 @@
 
 static NSString * mkNotFirstTime = @"mkNotFirstTime";
 
+static NSString * ADMOB_UNIT_ID = @"ca-app-pub-1577150706472824/4620795596";
+
 
 
---
+++ Mykeen/TableVC.h
@@ -0,0 +1,16 @@
+//
+//  TableVC.h
+//  Mykeen
+//
+//  Created by Ilia Kohanovski on 6/25/14.
+//  Copyright (c) 2014 Mykeen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "GADBannerView.h"
+
+@interface TableVC : UITableViewController
+
+@property (nonatomic, retain) GADBannerView *bannerView;
+
+@end
